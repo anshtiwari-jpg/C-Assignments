c9e7e1b5372929b853724ad12683d131d04784e0
Ansh Tiwari <ansh.tiwari@rungta.org>
2026-01-01 14:50:22 +0530

    Update Q03.c

--- Assignment_05/Q03.c
+++ Assignment_05/Q03.c
@@ -1,30 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    int arr[50];
-    int n, i, position;
+    char str[200];
+    int i, count = 1;
 
-    printf("Enter number of elements: ");
-    scanf("%d", &n);
+    printf("Enter a sentence:\n");
+    fgets(str, sizeof(str), stdin);
 
-    printf("Enter array elements:\n");
-    for (i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+    for(i = 0; str[i] != '\0'; i++) {
+        if(str[i] == ' ' && str[i + 1] != ' ') {
+            count++;
+        }
     }
 
-    printf("Enter position to delete (1 to %d): ", n);
-    scanf("%d", &position);
-
-    for (i = position - 1; i < n - 1; i++) {
-        arr[i] = arr[i + 1];
-    }
-
-    n--;
-
-    printf("\nArray after deletion:\n");
-    for (i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
+    printf("\nNumber of words = %d", count);
 
     return 0;
 }
